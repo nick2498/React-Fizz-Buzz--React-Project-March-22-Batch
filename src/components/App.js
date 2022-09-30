c5ec44858c302351a1a9422ca8e238dc3304f74b
Naitik Gupta <guptanaitik98@gmail.com>
2022-09-30 21:05:04 +0530

    update code

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,43 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useReducer } from "react";
+import { render } from "react-dom";
+import "../styles/App.css";
 const App = () => {
+  const initialState = 1;
 
-  
+  const counterReducer = (state, action) => {
+    switch (action) {
+      case "increment":
+        return state + 1;
+      case "decrement":
+        return state - 1;
+      default:
+        return 0;
+    }
+  };
+
+  const [state, disptch] = useReducer(counterReducer, initialState);
+
   return (
     <div id="main">
-
+      <button id="increment" onClick={() => disptch("increment")}>
+        Increment
+      </button>
+      <br />
+      <br />
+      <div id="counter">
+        {(state % 15 === 0 && <div className="fizzbuzz">{state}</div>) ||
+          (state % 5 === 0 && <div className="buzz">{state}</div>) ||
+          (state % 3 === 0 && <div className="fizz">{state}</div>) ||
+          (state % 3 !== 0 && state % 5 !== 0 && state % 15 !== 0 && (
+            <div className="normal">{state}</div>
+          ))}
+      </div>
+      <br />
+      <button id="decrement" onClick={() => disptch("decrement")}>
+        Decrement
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
